55aa05552eb030fdc118fbf7db213b47edfa36b8
knowlet <knowlet3389@gmail.com>
2014-04-16 12:57:19 +0800

    optimize function

--- 8ComicViewer.user.js
+++ 8ComicViewer.user.js
@@ -18,23 +18,23 @@ window.addEventListener("load", function() {
     exec(function() {
         var cc = cs.length;
         var content = document.getElementById("TheImg").parentNode;
-        content.removeChild(document.getElementById("TheImg"));
+        document.getElementById("TheImg").remove();
         for (var i = 1; i <= ps; ++i) {
             content.appendChild(document.createElement("img"));
-            content.getElementsByTagName("img")[i - 1].src = 'http://img' + ss(c, 4, 2) + '.8comic.com/' + ss(c, 6, 1) + '/' + ti + '/' + ss(c, 0, 4) + '/' + nn(i) + '_' + ss(c, mm(i) + 10, 3, f) + '.jpg';
+            content.getElementsByTagName("img")[i-1].src = 'http://img' + ss(c, 4, 2) + '.8comic.com/' + ss(c, 6, 1) + '/' + ti + '/' + ss(c, 0, 4) + '/' + nn(i) + '_' + ss(c, mm(i) + 10, 3, f) + '.jpg';
             content.appendChild(document.createElement("br"));
         }
         prev2.innerText = "[ 上一話 ]";
         prev2.onclick = function onclick(event) {
-            ch <= 1 && alert("前面沒有東西喔！")
+            ch <= 1 && alert("前面沒有東西喔！");
             pv();
-            return
+            return;
         }
         next2.innerText = "[ 下一話 ]";
         next2.onclick = function onclick(event) {
             nv();
             ch >= chs && alert("您已看完了！");
-            return
+            return;
         }
     });
 }, true);
