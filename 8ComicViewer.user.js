df6fb9efab9e1d4c5081c653e43ab344779de57a
knowlet <knowlet3389@gmail.com>
2014-04-16 18:23:45 +0800

    remove addEventListener

--- 8ComicViewer.user.js
+++ 8ComicViewer.user.js
@@ -14,27 +14,25 @@ function exec(fn) {
     document.body.removeChild(script);
 }
 
-window.addEventListener("load", function() {
-    exec(function() {
-        var cc = cs.length;
-        var content = document.getElementById("TheImg").parentNode;
-        document.getElementById("TheImg").remove();
-        for (var i = 1; i <= ps; ++i) {
-            content.appendChild(document.createElement("img"));
-            content.getElementsByTagName("img")[i-1].src = 'http://img' + ss(c, 4, 2) + '.8comic.com/' + ss(c, 6, 1) + '/' + ti + '/' + ss(c, 0, 4) + '/' + nn(i) + '_' + ss(c, mm(i) + 10, 3, f) + '.jpg';
-            content.appendChild(document.createElement("br"));
-        }
-        prev2.innerText = "[ 上一話 ]";
-        prev2.onclick = function onclick(event) {
-            ch <= 1 && alert("前面沒有東西喔！");
-            pv();
-            return;
-        }
-        next2.innerText = "[ 下一話 ]";
-        next2.onclick = function onclick(event) {
-            nv();
-            ch >= chs && alert("您已看完了！");
-            return;
-        }
-    });
-}, true);
+exec(function() {
+    var cc = cs.length;
+    var content = document.getElementById("TheImg").parentNode;
+    document.getElementById("TheImg").remove();
+    for (var i = 1; i <= ps; ++i) {
+        content.appendChild(document.createElement("img"));
+        content.getElementsByTagName("img")[i-1].src = 'http://img' + ss(c, 4, 2) + '.8comic.com/' + ss(c, 6, 1) + '/' + ti + '/' + ss(c, 0, 4) + '/' + nn(i) + '_' + ss(c, mm(i) + 10, 3, f) + '.jpg';
+        content.appendChild(document.createElement("br"));
+    }
+    prev2.innerText = "[ 上一話 ]";
+    prev2.onclick = function onclick(event) {
+        ch <= 1 && alert("前面沒有東西喔！");
+        pv();
+        return;
+    }
+    next2.innerText = "[ 下一話 ]";
+    next2.onclick = function onclick(event) {
+        nv();
+        ch >= chs && alert("您已看完了！");
+        return;
+    }
+});
