fb8cca51197b473b9e2fdacfb6241da4746be4b7
Knowlet <knowlet3389@gmail.com>
2014-04-21 00:26:42 +0800

    closure

--- 8ComicViewer.user.js
+++ 8ComicViewer.user.js
@@ -1,74 +1,62 @@
 // ==UserScript==
 // @name            8Comic Viewer
-// @version         1.22
+// @version         1.23
 // @author          knowlet
 // @namespace       http://knowlet3389.blogspot.tw/
 // @description     Auto load 8comic pic.
 // @match           http://new.comicvip.com/show/*
 // ==/UserScript==
-function exec(fn) {
-    var script = document.createElement('script');
-    script.setAttribute("type", "application/javascript");
-    script.textContent = '(' + fn + ')();';
-    document.body.appendChild(script);
-    document.body.removeChild(script);
-}
-
-exec(function() {
-    var content = document.getElementById("TheImg").parentNode,
-        cleanTable = function() {
-            var x = document.getElementsByTagName("table");
-            x[0].remove();
-            x[0].remove();
-            x[0].remove();
-            x[1].remove();
-            x[2].remove();
-            x[3].remove();
-        }, loadStyle = function() {
-            var style = document.createElement("link");
-            style.setAttribute("rel", "stylesheet")
-            style.setAttribute("type", "text/css")
-            style.setAttribute("href", "https://rawgit.com/knowlet/8Comic-Viewer/dev/css/style.css")
-            document.head.appendChild(style);
-        }, a = false,
-        setEvent = function() {
-            document.onmousemove = function(e) {
-                if (a) {
-                    nb.style.left = e.clientX - 20 + 'px';
-                    nb.style.top = e.clientY - 50 + 'px';
-                }
-            };
-            nb.onmousedown = function(e) {
-                a = true;
-            }
-            document.onmouseup = function(e) {
-                a = false;
-            }
-            btPrev.onclick = function onclick(event) {
-                ch <= 0 && alert("前面沒有東西喔！");
-                pv();
-                return;
-            }
-            btMenu.onclick = function onclick(event) {
-                location.assign("http://www.8comic.com/html/" + ti.toString() + ".html");
-                return;
-            }
-            btNext.onclick = function onclick(event) {
-                nv();
-                ch >= chs && alert("您已看完了！") && btMenu.click();
-                return;
-            }
-        }
-    document.getElementById("TheImg").remove();
-    localStorage.last > ch && confirm("你上次已經看到第" + localStorage.last + "話（卷）囉！\n是否要前往呢？") && jv(localStorage.last);
-    localStorage.setItem("last", ch);
-    for (var i = 1; i <= ps; ++i) {
-        content.appendChild(document.createElement("img"));
-        content.getElementsByTagName("img")[i - 1].setAttribute("src", 'http://img' + ss(c, 4, 2) + '.8comic.com/' + ss(c, 6, 1) + '/' + ti + '/' + ss(c, 0, 4) + '/' + nn(i) + '_' + ss(c, mm(i) + 10, 3, f) + '.jpg');
-        content.appendChild(document.createElement("br"));
-    }
-    cleanTable();
-    loadStyle();
-    document.body.innerHTML = Form1.innerHTML + "<nav id='nb'><ul><li id='btPrev'><img src='https://raw.githubusercontent.com/knowlet/8Comic-Viewer/dev/img/pv.png' alt='上一卷（話）'/></li><li id='btMenu'><img src='https://raw.githubusercontent.com/knowlet/8Comic-Viewer/dev/img/mu.png' alt='全集列表'/></li><li id='btNext'><img src='https://raw.githubusercontent.com/knowlet/8Comic-Viewer/dev/img/nv.png' alr='下一卷（話）'/></li></ul></nav>";
-    setEvent();
-});
+(function() {
+    var b = document.createElement("script");
+    b.setAttribute("type", "application/javascript");
+    b.textContent = "(" + function() {
+        var a = document.getElementById("TheImg").parentNode, b = false;
+        document.getElementById("TheImg").remove();
+        // Memory vol.
+        localStorage.a > ch && confirm("你上次已經看到第" + localStorage.last + "話（卷）囉！\n是否要前往呢？") && jv(localStorage.a);
+        localStorage.setItem("last", ch);
+        // Load pic
+        for (var d = 1; d <= ps; ++d)
+            a.appendChild(document.createElement("img")), a.getElementsByTagName("img")[d - 1].setAttribute("src", "http://img" + ss(c, 4, 2) + ".8comic.com/" + ss(c, 6, 1) + "/" + ti + "/" + ss(c, 0, 4) + "/" + nn(d) + "_" + ss(c, mm(d) + 10, 3, f) + ".jpg"), a.appendChild(document.createElement("br"));
+        // Clean table
+        a = document.getElementsByTagName("table");
+        a[0].remove();
+        a[0].remove();
+        a[0].remove();
+        a[1].remove();
+        a[2].remove();
+        a[3].remove();
+        // Load css
+        a = document.createElement("link");
+        a.setAttribute("rel", "stylesheet");
+        a.setAttribute("type", "text/css");
+        a.setAttribute("href", "https://rawgit.com/knowlet/8Comic-Viewer/dev/css/style.css");
+        document.head.appendChild(a);
+        // Create navbar
+        document.body.innerHTML = Form1.innerHTML + "<nav id='nb'><ul><li id='btPrev'><img src='https://raw.githubusercontent.com/knowlet/8Comic-Viewer/dev/img/pv.png' alt='上一卷（話）'/></li><li id='btMenu'><img src='https://raw.githubusercontent.com/knowlet/8Comic-Viewer/dev/img/mu.png' alt='全集列表'/></li><li id='btNext'><img src='https://raw.githubusercontent.com/knowlet/8Comic-Viewer/dev/img/nv.png' alr='下一卷（話）'/></li></ul></nav>";
+        // Drag events
+        document.onmousemove = function(a) {
+            b && (nb.style.left = a.clientX - 20 + "px", nb.style.top = a.clientY - 50 + "px");
+        };
+        nb.onmousedown = function() {
+            b = true;
+        };
+        document.onmouseup = function() {
+            b = false;
+        };
+        // Btn events
+        btPrev.onclick = function() {
+            0 >= ch && alert("前面沒有東西喔！");
+            pv();
+        };
+        btMenu.onclick = function() {
+            location.assign("http://www.8comic.com/html/" + ti.toString() + ".html");
+        };
+        btNext.onclick = function() {
+            nv();
+            ch >= chs && alert("您已看完了！") && btMenu.click();
+        };
+    } + ")();";
+    document.body.appendChild(b);
+    document.body.removeChild(b);
+})();
