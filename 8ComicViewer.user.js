ac13facbca57e01a464414138453307069d75dd7
Albert Hsieh <knowlet@users.noreply.github.com>
2019-04-23 20:40:45 +0800

    Update 8ComicViewer.user.js
    Fix #14

--- 8ComicViewer.user.js
+++ 8ComicViewer.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         8Comic Viewer
 // @namespace    https://knowlet3389.blogspot.tw/
-// @version      1.61
+// @version      1.62
 // @description  Auto load 8comic pic.
 // @author       KNowlet
 // @include      /^http[s]?\:\/\/www.comicvip.com\/show\/.*$/
 // @include      /^http[s]?\:\/\/v.comicbus.com\/online\/.*$/
 // @include      /^http[s]?\:\/\/v.nowcomic.com\/online\/.*$/
 // @include      /^http[s]?\:\/\/www.comicgood.com\/online\/.*$/
+// @include      /^http[s]?\:\/\/www.comicbus.cc\/online\/.*$/
 // @grant        none
 // @downloadURL  https://github.com/knowlet/8Comic-Viewer/raw/master/8ComicViewer.user.js
 // ==/UserScript==
